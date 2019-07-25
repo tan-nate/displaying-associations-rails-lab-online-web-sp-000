63af9fe0616968a109165ca9c97795bb96a1b12b
Nate Tan <nktan93@gmail.com>
2019-07-25 13:54:07 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -13,7 +13,7 @@ class ArtistsController < ApplicationController
     @artist = Artist.new(artist_params)
 
     if @artist.save
-      redirect_to @artist
+      redirect_to artist_path(@artist)
     else
       render :new
     end
